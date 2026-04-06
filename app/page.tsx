508bf27a25ba88f3a021f27c9ad8256e296fb3e9
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2026-04-06 12:05:17 +0530

    change in home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+
+     <div>
+      <a href="/auth/login" className='text-yellow-200 hover:text-orange-300 z-10 rounded-xl text-2xl'>Click here to login</a>
+    </div>
+    </>
   )
 }
 
